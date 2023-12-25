f0c42f4b0ba58aa0cb717c94c1a28ec6fb2185e9
Shashwat Verma <shashwatv18@gmail.com>
2023-12-25 21:21:01 +0530

    views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -7,12 +8,12 @@ const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not Found !</h1>')
+    res.status(404).sendFile(path.join(__dirname, '/views/404.html'))
 })
 
 app.listen(3000); 
